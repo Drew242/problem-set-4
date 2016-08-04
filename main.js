eb1d629dfba61cfbd724aed98ae5956dea76b12c
Drew Conly <atconly@gmail.com>
2016-08-04 16:24:37 -0600

    finish bonus problem

--- main.js
+++ main.js
@@ -16,3 +16,36 @@ var addNumbers = function(str) {
 addNumbers('88Hello 3World');
 addNumbers('55Hello');
 addNumbers('5Hello 5');
+
+var longestWord = function(str) {
+  var words  = str.split(' ');
+  var length = 0;
+  var word   = '';
+
+  for (i=0;i<words.length;i++) {
+    if(words[i].length > length){
+      length = words[i].length;
+      word = words[i];
+    }
+  }
+
+  console.log(`${word} === ${length}`);
+  return `${word} === ${length}`;
+}
+
+longestWord("This is a sentence");
+longestWord("If you're looking for a chupacabra, you're going to find a chupacabra");
+
+var averageStringNumbers = function(str) {
+  var lcount = '';
+  var chars  = str.split('');
+  for(i=0; i<chars.length; i++) {
+    if (!parseInt(chars[i])) {
+      lcount += chars[i];
+    }
+  }
+  var lcount = lcount.replace(/\s/g,'');
+  console.log(Math.round(addNumbers(str) / (lcount.length)));
+}
+
+averageStringNumbers('Hello6 9World 2, Nic8e D7ay!');
